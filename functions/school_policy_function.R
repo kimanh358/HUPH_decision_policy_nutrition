90dcc54d43292862a1e282241bf47f2f681105c0
CWWhitney <whitney.cory@gmail.com>
2025-04-04 13:47:23 +0200

    update with costs and benefits assigned to toggled management options

--- functions/school_policy_function.R
+++ functions/school_policy_function.R
@@ -2,20 +2,20 @@
 
 # modeling from intervention → practice → food environment → health → economic outcome
 
-# # make variables for testing our model (only for construction)
-# library(decisionSupport)
-# 
-# make_variables <- function(est, n = 1)
-#   #take one time #
-# {
-#   x <- decisionSupport::random(rho = est, n = n)
-#   for (i in colnames(x))
-#     assign(i, as.numeric(x[1, i]), envir = .GlobalEnv)
-# }
-# 
-# #Just one run#
-# make_variables(decisionSupport::estimate_read_csv(paste
-# ("data/inputs_school_policy.csv", sep ="")))
+# make variables for testing our model (only for construction)
+library(decisionSupport)
+
+make_variables <- function(est, n = 1)
+  #take one time #
+{
+  x <- decisionSupport::random(rho = est, n = n)
+  for (i in colnames(x))
+    assign(i, as.numeric(x[1, i]), envir = .GlobalEnv)
+}
+
+#Just one run#
+make_variables(decisionSupport::estimate_read_csv(paste
+("data/inputs_school_policy.csv", sep ="")))
 
 # Model function
 school_policy_function <- function(
@@ -24,52 +24,81 @@ school_policy_function <- function(
   # Apply toggles to adjust input parameters
   # transform to yes no binary
   # a probability around 0.5 (i.e., Bernoulli distribution)
-  use_staff_training_foodsafety <- chance_event(use_staff_training_foodsafety)
-  if (!use_staff_training_foodsafety) training_costs_foodsafety_annual <- 0
-  if (!use_staff_training_foodsafety) training_costs_foodsafety_1st_year <- 0
-  use_staff_training_nutrition <- chance_event(use_staff_training_nutrition)
-  if (!use_staff_training_nutrition) training_costs_nutrition_annual <- 0
-  if (!use_staff_training_nutrition) training_costs_nutrition_1st_year <- 0
+  # Staff training: Food safety
+  staff_training_foodsafety <- chance_event(use_staff_training_foodsafety)
+  if (!staff_training_foodsafety) {
+    # if not then no costs
+    training_costs_foodsafety_annual <- 0
+    training_costs_foodsafety_1st_year <- 0
+  } else {
+    n_reduce_disease_diagnosis <- n_reduce_disease_diagnosis * staff_training_foodsafety_nutrition_effect
+    n_reduce_disease_treatment <- n_reduce_disease_treatment * staff_training_foodsafety_nutrition_effect
+  }
+  
+  # Staff training: Nutrition
+  staff_training_nutrition <- chance_event(use_staff_training_nutrition)
+  if (!staff_training_nutrition) {
+    training_costs_nutrition_annual <- 0
+    training_costs_nutrition_1st_year <- 0
+  } else {
+    student_performance_improvement <- student_performance_improvement * staff_training_nutrition_effect
+    staff_knowledge_nutrition <- staff_knowledge_nutrition * staff_training_nutrition_effect
+  }
+  
+  # Physical education
   use_physical_activity <- chance_event(use_physical_activity)
-  if (!use_physical_activity) training_costs_physical_activity_annual <- 0
-  if (!use_physical_activity) training_costs_physical_activity_1st_year <- 0
+  if (!use_physical_activity) {
+    training_costs_physical_activity_annual <- 0
+    training_costs_physical_activity_1st_year <- 0
+  } else {
+    student_performance_improvement <- student_performance_improvement * physical_activity_effect
+    n_reduce_disease_diagnosis <- n_reduce_disease_diagnosis * physical_activity_effect
+    n_reduce_disease_treatment <- n_reduce_disease_treatment * physical_activity_effect
+    staff_knowledge_food_safety <- staff_knowledge_food_safety * physical_activity_effect
+  }
+  
+  # Menu change to meet RDA
   use_menu_change_rda <- chance_event(use_menu_change_rda)
-  if (!use_menu_change_rda) change_menu_costs_annual <- 0
+  if (!use_menu_change_rda) {
+    change_menu_costs_annual <- 0
+  } else {
+    n_reduce_disease_diagnosis <- n_reduce_disease_diagnosis * menu_change_rda_nutrition_effect
+    n_reduce_disease_treatment <- n_reduce_disease_treatment * menu_change_rda_nutrition_effect
+    unhealthy_canteen_foods <- unhealthy_canteen_foods * physical_activity_effect
+  }
+  
+  # Limit unhealthy canteen food
   use_limit_unhealthy_canteen_food <- chance_event(use_limit_unhealthy_canteen_food)
-  if (!use_limit_unhealthy_canteen_food) monitoring_canteen_cost <- 0
+  if (!use_limit_unhealthy_canteen_food) {
+    monitoring_canteen_cost <- 0
+  } else {
+    n_reduce_disease_diagnosis <- n_reduce_disease_diagnosis * limit_unhealthy_canteen_food_nutrition_effect
+    n_reduce_disease_treatment <- n_reduce_disease_treatment * limit_unhealthy_canteen_food_nutrition_effect
+    unhealthy_canteen_foods <- unhealthy_canteen_foods * physical_activity_effect
+  }
   
   # Composite indicators (simplified)
-  staff_knowledge_index <- mean(c(staff_knowledge_food_safety, staff_knowledge_nutrition))
-  meal_practices_score <- mean(c(meal_nutrition_practices_staff, food_safety_practices_staff))
   unhealthy_food_exposure <- max(c(unhealthy_canteen_foods, unhealthy_school_gate_foods, advertisement_exposure))
-  oversight_index <- mean(c(practice_school_management_board, parents_monitor_school_meal_practices))
   
   # Policy impacts (attenuated by barriers)
-  health_lessons_effective <- health_lessons_class * (1 - teaching_time)
-  staff_practice_effective <- meal_practices_score * (1 - resistance_existing_staff_practices)
   food_access_score <- children_access_healthy_food * (1 - unhealthy_food_exposure) * (1 - resistance_child_preferences_attitude)
   food_consumption_score <- children_consume_healthy_food * (1 - unhealthy_food_exposure) * (1 - resistance_child_preferences_attitude)
   
-  # Peer influence (bonus)
+  # Peer influence (negative)
   if (chance_event(peer_influence_factor)) {
-    food_consumption_score <- food_consumption_score * (1 + peer_influence_factor)
-  }
-  
-  # Physical activity program effect on BMI
-  if (physical_activity_program_effective == 1) {
-    bmi_risk_modifier <- 1 - physical_activity_effect
-  } else {
-    bmi_risk_modifier <- 1
+    food_consumption_score <- food_consumption_score * (1 - peer_influence_factor)
   }
   
   # BMI and disease outcomes
   if ((food_access_score + food_consumption_score) >= food_access_and_consumption_threshold) {
-    bmi_high <- nutrition_status_bmi_high * (1 - food_access_and_consumption_reduce_overweight) * bmi_risk_modifier
+    bmi_high <- nutrition_status_bmi_high * (1 - food_access_and_consumption_reduce_overweight) 
     bmi_low <- nutrition_status_bmi_low * (1 - food_access_and_consumption_reduce_underweight)
   } else {
     bmi_high <- nutrition_status_bmi_high
     bmi_low <- nutrition_status_bmi_low
   }
+  
+  disease_burden <- bmi_high - bmi_low
   
   # Health costs with and without policy
   # Health utility  ####
@@ -77,6 +106,8 @@ school_policy_function <- function(
   # How many fewer health events per VND spent?
   # A discussion among school nurses indicated that approximately 3-4% of the student body visits the school nurse each day
   # 50% to 70% of nurse visits result in some form of treatment or medication
+  baseline_disease_diagnosis <- baseline_disease_diagnosis * (1+disease_burden)
+  
   baseline_health_costs <- ((baseline_disease_diagnosis * disease_diagnosis_cost) + 
     (baseline_disease_treatment * disease_treatment_cost)) * n_student
   
@@ -118,7 +149,12 @@ school_policy_function <- function(
     list(
       decision_value = npv_policy - npv_no_policy,
       net_health_benefit = net_health_benefit,
-      education_benefit = education_benefit
+      education_benefit = education_benefit, 
+      use_staff_training_foodsafety = use_staff_training_foodsafety,
+      use_staff_training_nutrition = use_staff_training_nutrition,
+      use_physical_activity = use_physical_activity,
+      use_menu_change_rda = use_menu_change_rda,
+      use_limit_unhealthy_canteen_food = use_limit_unhealthy_canteen_food
     )
   )
 }
